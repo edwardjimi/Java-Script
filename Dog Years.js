494376211213e73560c3159556e5b5847be11b87
edwardjimi <90045447+edwardjimi@users.noreply.github.com>
2021-12-19 20:54:49 +0000

    Add files via upload

---
+++ Dog Years.js
@@ -0,0 +1,14 @@
+// The age of the user
+const myAge = ;
+
+//Early age of the user
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'James Edwards' .toLowerCase();
+
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
