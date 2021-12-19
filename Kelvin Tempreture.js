13f4ae00a4d3705dacd446dec1e662f2a2be8ae1
edwardjimi <90045447+edwardjimi@users.noreply.github.com>
2021-12-19 20:55:48 +0000

    Update Kelvin Tempreture.js

--- Kelvin Tempreture.js
+++ Kelvin Tempreture.js
@@ -4,14 +4,10 @@ const kelvin = 300;
 //Celsius temperture 
 const celsius = kelvin - 273;
 
-let newton = celsius * (33/100);
-
-newton = Math.floor (newton);
-
 //Fahrenheit temperture
 let fahrenheit = celsius * (9/5) + 32;
 
 //Rounding the decimal to a rounded number 
 fahrenheit = Math.floor (fahrenheit);
 
-console.log (`The temperature is ${newton} degrees Fahrenheit`)
+console.log (`The temperature is ${fahrenheit} degrees Fahrenheit`)
