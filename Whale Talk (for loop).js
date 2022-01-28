0807bbc09f426359cfbba1c07adbdffff728ddf1
James Edwards <90045447+edwardjimi@users.noreply.github.com>
2022-01-28 01:16:32 +0000

    Add files via upload

---
+++ Whale Talk (for loop).js
@@ -0,0 +1,24 @@
+const input = 'You will face many defeats in life, but never let yourself be defeated.';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const resultArray = [];
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++){
+  //console.log('Input Index = ' + input[inputIndex]);
+  for (let vowel = 0; vowel < vowels.length; vowel++){
+    if (input[inputIndex] === vowels[vowel]){
+      if (input[inputIndex] === 'e'){
+        resultArray.push('ee');
+      }
+      else if (input[inputIndex] === 'u'){
+        resultArray.push('uu');
+        }
+      else{
+        resultArray.push(input[inputIndex]);
+      }
+
+    }
+  }
+}
+
+console.log(resultArray.join('').toUpperCase())
